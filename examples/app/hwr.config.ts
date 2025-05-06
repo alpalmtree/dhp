7ab4f9fd9043a45ad13c7908748f441fb46fc3a3
Al <alpalma95@gmail.com>
2025-05-06 14:51:23 +0200

    chore: add example

---
+++ examples/app/hwr.config.ts
@@ -0,0 +1,22 @@
+import { Config } from "@timberstack/hwr";
+
+/**
+ * This file will only be generated only if it doesn't exists.
+ * It is an exact copy of the default config used by HWR.
+ * Feel free to remove the options that you don't need.
+ */
+
+export default {
+  // Folder to be used as root for the router
+  viewsDir: "/views",
+  // Folder from where to serve public assets. It will also be the outDir for vite
+  publicDir: "/public",
+  // Serve the routes directly from your root folder by default. Change it to true if you want to use the generated router file instead.
+  precompile: false,
+  // Folder where the generated files will live
+  routerPath: "/file-router",
+  // You can opt out from vite by setting this to false
+  useVite: true,
+  // Entry points for vite build
+  resourcesPath: "/resources/entrypoints",
+} satisfies Config;
