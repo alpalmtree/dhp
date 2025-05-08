675e039cbb59769da568583706ac4ab8675acc4e
Al <alpalma95@gmail.com>
2025-05-08 19:27:50 +0200

    link dist to example app

--- scripts/build.ts
+++ scripts/build.ts
@@ -6,6 +6,7 @@ await tsup.build({
     `${Deno.cwd()}/src/cli.ts`,
     `${Deno.cwd()}/src/ViteHead.tsx`,
   ],
+  noExternal: ["@timberstack/vite-plugin-better-manifest"],
   tsconfig: `${Deno.cwd()}/scripts/build.tsconfig.json`,
   format: "esm",
   dts: true,
