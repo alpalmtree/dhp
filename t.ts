a970eeca813eab390daa873c308b301efcd6b103
Al <alpalma95@gmail.com>
2025-05-13 09:07:38 +0200

    test

--- t.ts
+++ t.ts
@@ -1,3 +1,7 @@
 import { path } from "./src/vendor/std.ts";
 
-console.log(new URL(import.meta.url));
+console.log("just import meta url:", new URL(import.meta.url));
+console.log(
+  "with reference to a file:",
+  new URL(import.meta.resolve("./src/cli.ts")),
+);
